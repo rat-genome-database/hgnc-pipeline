be46478319e38b1777a5aa4c355321bed2a7cc98
llamersmcw <58486178+llamersmcw@users.noreply.github.com>
2020-05-26 12:36:29 -0500

    bash file to run hgnc pipeline for mgi data

---
+++ src/main/dist/processMgiData.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# shell script to run Hgnc pipeline
+. /etc/profile
+
+APPNAME=HgncPipeline
+APPDIR=/home/rgddata/pipelines/$APPNAME
+SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
+
+EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu
+if [ "$SERVER" == "REED" ]; then
+  EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu,jrsmith@mcw.edu,jdepons@mcw.edu
+fi
+cd $APPDIR
+
+java -Dspring.config=$APPDIR/../properties/default_db2.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/$APPNAME.jar --processMgiData > $APPDIR/hgncIds.log
+
+
+mailx -s "[$SERVER] HgncDataPipeline OK!" $EMAILLIST < $APPDIR/logs/hgnc_ids_summary.log
