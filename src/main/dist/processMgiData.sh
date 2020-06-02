c7b48667fed5c6e1b8926694ffc24be9090d6c5b
llamersmcw <58486178+llamersmcw@users.noreply.github.com>
2020-06-02 08:37:06 -0500

    updated email to send correct log file

--- src/main/dist/processMgiData.sh
+++ src/main/dist/processMgiData.sh
@@ -15,7 +15,7 @@ cd $APPDIR
 
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
-    -jar lib/$APPNAME.jar --processMgiData > $APPDIR/logs/mgi_logger.log
+    -jar lib/$APPNAME.jar --processMgiData > $APPDIR/mgi_logger.log
 
 
-mailx -s "[$SERVER] HgncDataPipeline OK!" $EMAILLIST < $APPDIR/logs/hgnc_ids_summary.log
+mailx -s "[$SERVER] HgncDataPipeline OK!" $EMAILLIST < $APPDIR/logs/mgi_logger.log
