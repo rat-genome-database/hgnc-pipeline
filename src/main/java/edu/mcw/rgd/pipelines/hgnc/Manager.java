dc0394017d2c2b3c72625fdb69305ef330dee9de
tutajm <mtutaj@mcw.edu>
2023-02-14 08:24:15 -0600

    updated build.gradle to be gradle 6.x + compatible

--- src/main/java/edu/mcw/rgd/pipelines/hgnc/Manager.java
+++ src/main/java/edu/mcw/rgd/pipelines/hgnc/Manager.java
@@ -1,11 +1,20 @@
 package edu.mcw.rgd.pipelines.hgnc;
 
+import edu.mcw.rgd.dao.impl.GeneDAO;
+import edu.mcw.rgd.datamodel.Gene;
+import edu.mcw.rgd.datamodel.MappedGene;
 import edu.mcw.rgd.process.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.FileSystemResource;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author mtutaj
@@ -22,6 +31,7 @@ public class Manager {
         new XmlBeanDefinitionReader(bf).loadBeanDefinitions(new FileSystemResource("properties/AppConfigure.xml"));
 
         try {
+            test();
 
             // process cmdline params
             for( int i=0; i<args.length; i++ ) {
@@ -48,5 +58,48 @@ public class Manager {
             Utils.printStackTrace(e, logger);
             throw e;
         }
+    }
+
+    static void test() throws Exception {
+
+        BufferedReader in = Utils.openReader("/tmp/snpEff_genes_joint72.txt");
+        BufferedWriter out = Utils.openWriter("/tmp/snpEff_genes_joint72.out");
+
+        GeneDAO dao = new GeneDAO();
+
+        int lines = 0;
+        String line;
+        while( (line=in.readLine())!=null ) {
+            lines++;
+            int tabPos = line.indexOf('\t');
+            if( tabPos > 0 ) {
+                String chr = "";
+                String geneSymbol = line.substring(0, tabPos);
+                System.out.println(lines+". "+geneSymbol);
+                List<String> geneSymbols = new ArrayList<>(1);
+                geneSymbols.add(geneSymbol);
+                List<MappedGene> genes = dao.getActiveMappedGenes(372, geneSymbols);
+                if( genes.isEmpty() ) {
+                    System.out.println("Cannot resolve symbol: "+geneSymbol);
+                } else if( genes.size()>1 ) {
+                    System.out.println("Multiple genes: "+geneSymbol);
+                } else {
+                    chr = genes.get(0).getChromosome();
+                }
+                out.write(chr);
+                out.write('\t');
+                out.write(line);
+                out.write("\n");
+            }
+            else {
+                out.write(line);
+                out.write("\n");
+            }
+        }
+
+        in.close();
+        out.close();
+
+        System.exit(0);
     }
 }
