46a411443afc9051c2d667a72761bd8e21712239
tutajm <mtutaj@mcw.edu>
2023-04-03 13:33:12 -0500

    removed Harika from email list

--- src/main/dist/processHgncIds.sh
+++ src/main/dist/processHgncIds.sh
@@ -5,9 +5,9 @@ APPNAME="hgnc-pipeline"
 APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 
-EMAILLIST=hsnalabolu@mcw.edu,mtutaj@mcw.edu
+EMAILLIST=mtutaj@mcw.edu
 if [ "$SERVER" == "REED" ]; then
-  EMAILLIST=hsnalabolu@mcw.edu,mtutaj@mcw.edu,jrsmith@mcw.edu,jdepons@mcw.edu
+  EMAILLIST=mtutaj@mcw.edu,jrsmith@mcw.edu,jdepons@mcw.edu
 fi
 cd $APPDIR
 
