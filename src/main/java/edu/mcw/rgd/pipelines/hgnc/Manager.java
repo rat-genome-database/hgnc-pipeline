290c1fe2d5d64c1aa6f364fa7c36d897c5ae5359
tutajm <mtutaj@mcw.edu>
2024-02-12 17:24:03 -0600

    simplified logging

--- src/main/java/edu/mcw/rgd/pipelines/hgnc/Manager.java
+++ src/main/java/edu/mcw/rgd/pipelines/hgnc/Manager.java
@@ -1,20 +1,10 @@
 package edu.mcw.rgd.pipelines.hgnc;
 
-import edu.mcw.rgd.dao.impl.GeneDAO;
-import edu.mcw.rgd.datamodel.Gene;
-import edu.mcw.rgd.datamodel.MappedGene;
 import edu.mcw.rgd.process.Utils;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.FileSystemResource;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author mtutaj
@@ -23,12 +13,12 @@ import java.util.List;
  */
 public class Manager {
 
-    static Logger logger = LogManager.getLogger("status");
-
     public static void main(String[] args) throws Exception {
 
         DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
         new XmlBeanDefinitionReader(bf).loadBeanDefinitions(new FileSystemResource("properties/AppConfigure.xml"));
+
+        Logger logger = null;
 
         try {
             // process cmdline params
@@ -36,70 +26,33 @@ public class Manager {
                 switch(args[i]) {
                     case "--processObsoleteHgncIds": {
                         ObsoleteHgncIdManager manager = (ObsoleteHgncIdManager) (bf.getBean("obsoleteHgncIdManager"));
+                        logger = manager.logDb;
                         manager.run();
-                        return;
+                        break;
                     }
                     case "--processHgncIds": {
                         HgncIdManager manager = (HgncIdManager) (bf.getBean("hgncIdManager"));
+                        logger = manager.logDb;
                         manager.run();
-                        return;
+                        break;
                     }
                     case "--processMgiData": {
                         MgiManager manager = (MgiManager) (bf.getBean( "MgiManager"));
+                        logger = manager.logger;
                         manager.run();
-                        return;
+                        break;
                     }
                 }
             }
 
         } catch(Exception e) {
-            Utils.printStackTrace(e, logger);
+            if( logger != null ) {
+                Utils.printStackTrace(e, logger);
+            } else {
+                e.printStackTrace();
+            }
             throw e;
         }
     }
 
-    /*
-    static void test() throws Exception {
-
-        BufferedReader in = Utils.openReader("/tmp/snpEff_genes_joint72.txt");
-        BufferedWriter out = Utils.openWriter("/tmp/snpEff_genes_joint72.out");
-
-        GeneDAO dao = new GeneDAO();
-
-        int lines = 0;
-        String line;
-        while( (line=in.readLine())!=null ) {
-            lines++;
-            int tabPos = line.indexOf('\t');
-            if( tabPos > 0 ) {
-                String chr = "";
-                String geneSymbol = line.substring(0, tabPos);
-                System.out.println(lines+". "+geneSymbol);
-                List<String> geneSymbols = new ArrayList<>(1);
-                geneSymbols.add(geneSymbol);
-                List<MappedGene> genes = dao.getActiveMappedGenes(372, geneSymbols);
-                if( genes.isEmpty() ) {
-                    System.out.println("Cannot resolve symbol: "+geneSymbol);
-                } else if( genes.size()>1 ) {
-                    System.out.println("Multiple genes: "+geneSymbol);
-                } else {
-                    chr = genes.get(0).getChromosome();
-                }
-                out.write(chr);
-                out.write('\t');
-                out.write(line);
-                out.write("\n");
-            }
-            else {
-                out.write(line);
-                out.write("\n");
-            }
-        }
-
-        in.close();
-        out.close();
-
-        System.exit(0);
-    }
-    */
 }
